fdc56b933e845200d5dd43ea71c816fd3ceb0698
franciscolag <francisco.silva@mota-engil.com.mx>
2022-08-15 18:31:18 -0500

    Agregar Cors

--- public/index.php
+++ public/index.php
@@ -15,7 +15,6 @@ require '../src/Models/Usuarios.php';
 require '../src/login/Auth.php';
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers:X-Request-With');
-
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
